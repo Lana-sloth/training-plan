abaab3e8747997689579f08213e50d2bf672f5a8
Lana-sloth <lana.sloth@gmail.com>
2018-02-28 08:45:09 +0300

    Shows list of workouts for a week, highlights workout for today

--- src/app/habits/habits.component.ts
+++ src/app/habits/habits.component.ts
@@ -4,26 +4,29 @@ import { Component } from '@angular/core';
   selector: 'habits',
   styleUrls: ['../app.component.css'],
   template: `
-    <div>{{ workout.title }}</div>
-    <div>Today's routine: {{ workout.exersises[today] }}</div>
-    <div></div>
+    <h1>{{ workout.title }}</h1>
+    <div 
+    *ngFor='let exersise of workout.exersises'
+    [style.color]='(getDayOfWeek(exersise.index) ? "#000":"#ccc")'>
+        {{ exersise.day }}: {{ exersise.workout }}
+    </div>
   `
 })
-export class HabitsComponent {
-    today = this.getDayOfWeek();
-    getDayOfWeek(){
-        return new Date().getDay() - 1;
+export class HabitsComponent { 
+    getDayOfWeek(day){
+        let today = new Date().getDay();
+        if (today == day) return true;
     }
     assassin = {
         title: 'Assassin',
         exersises: [
-            'High Birn / HIIT workout',
-            'Rest / casual training',
-            'Combat Workout',
-            'High Birn / HIIT workout',
-            'Rest / casual training',
-            'High Birn / HIIT workout',
-            'Rest / casual training'
+            {workout:'High Birn / HIIT workout', day:'Mo', index:1},
+            {workout:'Rest / casual training', day:'Tu', index:2},
+            {workout:'Combat Workout', day:'We', index:3},
+            {workout:'High Birn / HIIT workout', day:'Th', index:4},
+            {workout:'Rest / casual training', day:'Fr', index:5},
+            {workout:'High Birn / HIIT workout', day:'Sa', index:6},
+            {workout:'Rest / casual training', day:'Su', index:7}
         ]
     }
     workout = this.assassin;
