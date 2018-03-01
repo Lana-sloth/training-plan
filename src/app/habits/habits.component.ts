bc8abff051fcc7ce1cfab0e99d07f7e08d416216
Lana-sloth <lana.sloth@gmail.com>
2018-03-01 17:00:12 +0300

    Changed links display, two activities can be chosen in one day

--- src/app/habits/habits.component.ts
+++ src/app/habits/habits.component.ts
@@ -11,12 +11,20 @@ import { Workout } from './models/habits.models';
         <h1>{{ workout.title | titlecase }}</h1>
         <div 
         *ngFor='let exersise of workout.exersises'
-        [style.color]='(getDayOfWeek(exersise.index) ? "#000":"#ccc")'>
-            {{ exersise.day }}: {{ exersise.workout.name }} 
+        [style.backgroundColor]='(getDayOfWeek(exersise.index) ? "#ccc":"#fff")'>
+            {{ exersise.day }}:  
 
-            <a 
-            *ngIf='getDayOfWeek(exersise.index)'
-            href='{{ exersise.workout.link }}'> link </a>
+            <span *ngIf='!exersise.workout.combine'>
+                <a *ngIf='exersise.workout.link' href='{{ exersise.workout.link }}'>{{ exersise.workout.name }}</a>
+                <span *ngIf='!exersise.workout.link'>{{ exersise.workout.name }}</span>
+            </span>
+            
+            <span *ngIf='exersise.workout.combine'>
+                <a *ngIf='exersise.workout.firstLink' href='{{ exersise.workout.firstLink }}'>{{ exersise.workout.firstName }}</a>
+                <span *ngIf='!exersise.workout.firstLink'>{{ exersise.workout.firstName }}</span> or
+                <a *ngIf='exersise.workout.secondLink' href='{{ exersise.workout.secondLink }}'>{{ exersise.workout.secondName }}</a>
+                <span *ngIf='!exersise.workout.secondLink'>{{ exersise.workout.secondName }}</span>
+            </span>
 
         </div>
     </div>
@@ -27,113 +35,45 @@ export class HabitsComponent {
         let today = new Date().getDay();
         if (today == day) return true;
     }
-    activities = {
-        strength: {name:'Strength workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=strength&page=1'},
-        highBirn: {name:'High Birn workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=cardio&attr.ct14.value=normal&page=1'},
-        hiit: {name:'HIIT workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=hiit&page=1'},
-        highBirnAndHiit: {name:'High Birn / HIIT workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&page=1'},
-        restCasual: {name:'Rest / casual training', link:'https://darebee.com/fitness/casual-training.html'},
-        highBirnCombat: {name:'High Birn / Combat workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&page=1'},
-        combat: {name:'Combat workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=combat&page=1'},
-        upper: {name:'Upperbody workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct10.value=upper-body&page=1'},
-        lower: {name:'Lowerbody workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct10.value=lower-body&page=1'},
-        str: {name:'', link:''}
+    combine(a, b){
+        // if(a.link) {
+        //     return {
+        //         name: a.name,
+        //         link: a.link
+        //     }
+        // }
+        let list = this.activities
+        return {
+            combine: true,
+            firstName: list[a].name,
+            secondName: list[b].name,
+            firstLink: list[a].link,
+            secondLink: list[b].link
+        }
     }
+    activities = [
+        {name:'Strength workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=strength&page=1'},
+        {name:'High Birn workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=cardio&attr.ct14.value=normal&page=1'},
+        {name:'HIIT workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=hiit&page=1'},
+        {name:'High Birn / HIIT workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&page=1'},
+        {name:'Rest / casual training', link:'https://darebee.com/fitness/casual-training.html'},
+        {name:'High Birn / Combat workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&page=1'},
+        {name:'Combat workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct16.value=combat&page=1'},
+        {name:'Upperbody workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct10.value=upper-body&page=1'},
+        {name:'Lowerbody workout', link:'https://darebee.com/wods.html#sort=position&sortdir=desc&attr.ct10.value=lower-body&page=1'},
+        {name:'Rest', link:''}
+    ]
     workouts: Workout[] = [
         {
             title: 'guardian',
             exersises: [
-                {workout: this.activities.strength, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.highBirnAndHiit, day:'We', index:3},
-                {workout: this.activities.restCasual, day:'Th', index:4},
-                {workout: this.activities.strength, day:'Fr', index:5},
-                {workout: this.activities.restCasual, day:'Sa', index:6},
-                {workout: this.activities.restCasual, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'gladiator',
-            exersises: [
-                {workout: this.activities.strength, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.upper, day:'We', index:3},
-                {workout: this.activities.lower, day:'Th', index:4},
-                {workout: this.activities.restCasual, day:'Fr', index:5},
-                {workout: this.activities.strength, day:'Sa', index:6},
-                {workout: this.activities.restCasual, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'assassin',
-            exersises: [
-                {workout: this.activities.highBirnAndHiit, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.combat, day:'We', index:3},
-                {workout: this.activities.highBirnAndHiit, day:'Th', index:4},
-                {workout: this.activities.restCasual, day:'Fr', index:5},
-                {workout: this.activities.highBirnAndHiit, day:'Sa', index:6},
-                {workout: this.activities.restCasual, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'amazon',
-            exersises: [
-                {workout: this.activities.strength, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.highBirn, day:'We', index:3},
-                {workout: this.activities.restCasual, day:'Th', index:4},
-                {workout: this.activities.strength, day:'Fr', index:5},
-                {workout: this.activities.restCasual, day:'Sa', index:6},
-                {workout: this.activities.highBirn, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'fighter',
-            exersises: [
-                {workout: this.activities.combat, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.hiit, day:'We', index:3},
-                {workout: this.activities.strength, day:'Th', index:4},
-                {workout: this.activities.restCasual, day:'Fr', index:5},
-                {workout: this.activities.combat, day:'Sa', index:6},
-                {workout: this.activities.restCasual, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'ninja',
-            exersises: [
-                {workout: this.activities.highBirn, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.highBirn, day:'We', index:3},
-                {workout: this.activities.restCasual, day:'Th', index:4},
-                {workout: this.activities.highBirnCombat, day:'Fr', index:5},
-                {workout: this.activities.restCasual, day:'Sa', index:6},
-                {workout: this.activities.restCasual, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'knight',
-            exersises: [
-                {workout: this.activities.strength, day:'Mo', index:1},
-                {workout: this.activities.restCasual, day:'Tu', index:2},
-                {workout: this.activities.strength, day:'We', index:3},
-                {workout: this.activities.restCasual, day:'Th', index:4},
-                {workout: this.activities.strength, day:'Fr', index:5},
-                {workout: this.activities.restCasual, day:'Sa', index:6},
-                {workout: this.activities.restCasual, day:'Su', index:7}
-            ]
-        },
-        {
-            title: 'runner',
-            exersises: [
-                {workout:'Running', day:'Mo', index:1},
-                {workout:'Rest / light recovery run', day:'Tu', index:2},
-                {workout:'Upperbody workout', day:'We', index:3},
-                {workout:'Running', day:'Th', index:4},
-                {workout:'Rest / light recovery run', day:'Fr', index:5},
-                {workout:'Running', day:'Sa', index:6},
-                {workout:'Rest', day:'Su', index:7}
+                {workout: this.activities[0], day:'Mo', index:1},
+                {workout: this.combine(9, 2), day:'Tu', index:2},
+                {workout: this.activities[1], day:'We', index:3},
+                {workout: this.combine(9, 2), day:'Th', index:4},
+                {workout: this.activities[0], day:'Fr', index:5},
+                {workout: this.activities[0], day:'Sa', index:6},
+                {workout: this.activities[0], day:'Su', index:7}
             ]
         }
     ]
